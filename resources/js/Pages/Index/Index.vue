e993bcd272392869f7c212d6e8abc2644a2478a6
LaupWing <loc_strict@hotmail.com>
2023-03-24 05:48:34 +0100

    update

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+   <div>
+      Index
+      <Link href="/hello">Show page</Link>
+   </div>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3"
+
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+      Show
+      <Link href="/">Index page</Link>
+   </div>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3"
+</script>
