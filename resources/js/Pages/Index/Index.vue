1c994153dfa4af9c821542a9de0417b091545d98
LaupWing <loc_strict@hotmail.com>
2023-03-27 05:33:39 +0200

    33

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -14,10 +14,3 @@ import {Link} from "@inertiajs/vue3"
 const props = defineProps(["message"])
 
 </script>
-
-<script>
-import MainLayout from "../../Layouts/MainLayout.vue"
-export default {
-   layout: MainLayout
-}
-</script>
