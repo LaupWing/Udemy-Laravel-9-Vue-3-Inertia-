1e5231613d3694d05977922a8279ab4148a7dbdc
LaupWing <loc_strict@hotmail.com>
2023-03-30 07:01:42 +0200

    49

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,11 @@
 <template>
    <Link href="/hello">Show page</Link>
    <Link href="/">Index page</Link>
-   <slot></slot>
+   <slot>Default</slot>
 </template>
 
 <script setup>
 import {Link} from "@inertiajs/vue3"
 import { ref } from "vue"
-
-const timer = ref(0)
-setInterval(() => {
-   timer.value++
-}, 1000)
 
 </script>
