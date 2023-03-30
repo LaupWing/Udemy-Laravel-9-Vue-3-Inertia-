be7e696fa9644bcdf5dfa57e0a73871f0e26ad77
LaupWing <loc_strict@hotmail.com>
2023-03-30 07:13:30 +0200

    50

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,25 @@
 <template>
    <Link href="/hello">Show page</Link>
    <Link href="/">Index page</Link>
+   <div v-if="flashMessage" class="success">
+      {{ flashMessage }}
+   </div>
    <slot>Default</slot>
 </template>
 
 <script setup>
-import {Link} from "@inertiajs/vue3"
-import { ref } from "vue"
+import { Link, usePage } from "@inertiajs/vue3"
+import { computed, ref } from "vue"
+
+const page = usePage()
+
+const flashMessage = computed(() => page.props.flash?.success)
 
 </script>
+
+<style scoped>
+.success {
+   background-color: green;
+   color: white;
+}
+</style>
