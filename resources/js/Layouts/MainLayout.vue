b27d07ffdfba96d7a0742be11cb47dcd10063c81
LaupWing <loc_strict@hotmail.com>
2023-04-01 06:53:59 +0200

    removed creat ebutton

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,4 @@
 <template>
-   <Link href="/listing/create">Create new listing</Link>
    <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
       <div class="container mx-auto">
          <nav class="p-4 flex items-center justify-between">
