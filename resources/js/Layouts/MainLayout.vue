be637ccfa88b1314a9914af449d0193a3563f50b
LaupWing <loc_strict@hotmail.com>
2023-04-01 06:42:30 +0200

    58

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,10 +1,26 @@
 <template>
-   <Link href="/listing">Listings</Link>
    <Link href="/listing/create">Create new listing</Link>
-   <div v-if="flashMessage" class="success">
-      {{ flashMessage }}
-   </div>
-   <slot>Default</slot>
+   <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
+      <div class="container mx-auto">
+         <nav class="p-4 flex items-center justify-between">
+            <div class="text-lg font-medium">
+               <Link :href="route('listing.index')">Listings</Link>
+            </div>
+            <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+               <Link :href="route('listing.index')">LaraZillow</Link>
+            </div>
+            <div>
+               <Link class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md" :href="route('listing.create')">Create</Link>
+            </div>
+         </nav>
+      </div>
+   </header>
+   <main class="container p-4 mx-auto">
+      <div v-if="flashMessage" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+         {{ flashMessage }}
+      </div>
+      <slot>Default</slot>
+   </main>
 </template>
 
 <script setup>
@@ -15,10 +31,3 @@ import { computed, ref } from "vue"
 const flashMessage = computed(() => usePage().props.flash?.success)
 
 </script>
-
-<style scoped>
-.success {
-   background-color: green;
-   color: white;
-}
-</style>
