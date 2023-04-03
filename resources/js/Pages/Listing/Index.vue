74ea018808f4865464e3ac1898337b3b6d10ddfb
LaupWing <loc_strict@hotmail.com>
2023-04-03 05:56:50 +0200

    72

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,32 +1,15 @@
 <template>
    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-      <Box 
+      <Listing
          v-for="listing in listings"
          :key="listing.id"
-      >
-         <div>
-            <Link :href="route('listing.show', listing.id)">
-               <Price :price="listing.price" class="text-2xl font-bold"/>
-               <ListingSpace :listing="listing" />
-               <ListingAddress :listing="listing" class="text-gray-500" />
-            </Link>
-         </div>
-         <div>
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>
-         </div>
-         <div>
-            <Link as="button" :href="route('listing.destroy', listing.id)" method="DELETE">Delete</Link>
-         </div>
-      </Box>
+         :listing="listing"
+      />
    </div>
 </template>
 
 <script setup>
-import ListingAddress from "@/Components/ListingAddress.vue"
-import Price from "@/Components/Price.vue"
-import Box from "@/Components/UI/Box.vue"
-import ListingSpace from "@/Components/UI/ListingSpace.vue"
-import { Link } from "@inertiajs/vue3"
+import Listing from "./Index/Components/Listing.vue"
 defineProps({
    "listings": Array
 })
