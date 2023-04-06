d7010a793ec37715df37d093a7af637e832c025f
LaupWing <loc_strict@hotmail.com>
2023-04-06 05:31:10 +0200

    92

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,12 @@ class DatabaseSeeder extends Seeder
       // \App\Models\User::factory(10)->create();
 
       \App\Models\User::factory()->create([
-          'name' => 'Test User',
-          'email' => 'test@example.com',
+         'name' => 'Test User',
+         'email' => 'test@example.com',
+      ]);
+      \App\Models\User::factory()->create([
+         'name' => 'Test2 User',
+         'email' => 'test2@example.com',
       ]);
       \App\Models\Listing::factory(20)->create([
          "by_user_id" => 1
