5209010c9bdc21908777e66704ffdd0cb497e9ef
LaupWing <loc_strict@hotmail.com>
2023-04-09 10:35:07 +0200

    udpate

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -3,6 +3,7 @@
       <div class="mb-4 mt-4 flex flex-wrap gap-2">
          <div class="flex flex-nowrap items-center gap-2">
             <input 
+               v-model="filterForm.deleted"
                id="deleted" 
                type="checkbox" 
                class="w-4 h-4 border-gray-300 text-indigo-600 focus:ring-indigo-500" 
@@ -14,5 +15,19 @@
 </template>
 
 <script setup>
+import { reactive, watch } from "vue"
+import { router } from "@inertiajs/vue3"
 
+const filterForm = reactive({
+   deleted: false,
+})
+
+watch(() => router.get(
+   route("realtor.listing.index"),
+   filterForm,
+   {
+      preserveState: true,
+      
+   }
+))
 </script>
