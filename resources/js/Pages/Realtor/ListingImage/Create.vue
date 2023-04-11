5840c59200b569c4ff13b83b544bdaf61eef2f63
LaupWing <loc_strict@hotmail.com>
2023-04-11 05:55:36 +0200

    129

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -7,13 +7,28 @@
          method="POST"
          @submit.prevent="upload"
       >
-         <input 
-            type="file" 
-            multiple
-            @input="addFiles"
-         />
-         <button class="btn-outline" type="submit">Send</button>
-         <button class="btn-outline" @click="reset" type="reset">Reset</button>
+         <section class="flex items-center gap-2 my-4">
+            <input 
+               class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 dark:file:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:dark-gray-700 file:hover:cursor-pointer file:mr-4"
+               type="file" 
+               multiple
+               @input="addFiles"
+            />
+            <button 
+               class="btn-outline disabled:opacity-30 disabled:cursor-not-allowed" 
+               type="submit"
+               :disabled="canUpload"
+            >
+               Upload
+            </button>
+            <button 
+               class="btn-outline" 
+               @click="reset" 
+               type="reset"
+            >
+               Reset
+            </button>
+         </section>
       </form>
    </Box>
 </template>
@@ -21,6 +36,7 @@
 <script setup>
 import Box from "@/Components/UI/Box.vue"
 import { useForm } from "@inertiajs/vue3"
+import { computed } from "vue";
 
 const props = defineProps({
    listing: Object
@@ -29,6 +45,8 @@ const props = defineProps({
 const formData = useForm({
    images: []
 })
+
+const canUpload = computed(() => formData.images.length)
 
 const upload = () => {
    formData.post(
