4643a284d3dd6b8dc414390ea1a0092e7dbc7833
LaupWing <loc_strict@hotmail.com>
2023-04-11 05:50:21 +0200

    128

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,20 +1,51 @@
 <template>
-   <form 
-      enctype="multipart/form-data"
-      method="POST" 
-      :action="route('realtor.listing.image.store', {listing: listing.id})"
-   >
-      <input 
-         type="file" 
-         multiple 
-         name="images" 
-      />
-      <button type="submit">Send</button>
-   </form>
+   <Box>
+      <template #header>
+         Upload new Images
+      </template>
+      <form 
+         method="POST"
+         @submit.prevent="upload"
+      >
+         <input 
+            type="file" 
+            multiple
+            @input="addFiles"
+         />
+         <button class="btn-outline" type="submit">Send</button>
+         <button class="btn-outline" @click="reset" type="reset">Reset</button>
+      </form>
+   </Box>
 </template>
 
 <script setup>
-defineProps({
+import Box from "@/Components/UI/Box.vue"
+import { useForm } from "@inertiajs/vue3"
+
+const props = defineProps({
    listing: Object
 })
+
+const formData = useForm({
+   images: []
+})
+
+const upload = () => {
+   formData.post(
+      route("realtor.listing.image.store", {
+         listing: props.listing.id
+      }),
+      {
+         onSuccess: () => formData.reset("images")
+      }
+   )
+}
+
+const addFiles = (event) => {
+   for(const image of event.target.files) {
+      formData.images.push(image)
+   }
+}
+
+const reset = () => formData.reset("images")
 </script>
