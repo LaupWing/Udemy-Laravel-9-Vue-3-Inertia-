136984c52dae97a1af3bc216e0506f7f994a72a7
LaupWing <loc_strict@hotmail.com>
2023-04-13 08:51:39 +0200

    145

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -13,7 +13,7 @@
             </div>
 
             <div class="text-gray-500 text-sm">
-               Made by  John Doe
+               Made by  {{ offer.bidder }}
             </div>
 
             <div class="text-gray-500 text-sm">
