e491b4722e06898855d595f79851127246779c55
LaupWing <loc_strict@hotmail.com>
2023-04-14 05:24:46 +0200

    147

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -47,6 +47,16 @@ class Listing extends Model
       return $query->orderbyDesc("created_at");
    }
 
+   public function scopeWithoutSold(Builder $query): Builder {
+      return $query
+         ->doesntHave("offers")
+         ->orWhereHas("offers", fn(Builder $query) => 
+            $query
+               ->whereNull("accepted_at")
+               ->whereNull("rejected_at")
+         );
+   }
+
    public function scopeFilter(Builder $query, array $filters){
       return $query
          ->when(
