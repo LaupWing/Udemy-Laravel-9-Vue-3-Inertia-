bf28a517d577f1746e0e228c76d2dd4feea8bbb9
LaupWing <loc_strict@hotmail.com>
2023-04-14 05:42:38 +0200

    149

--- app/Http/Controllers/RealtorListingAcceptOfferController.php
+++ app/Http/Controllers/RealtorListingAcceptOfferController.php
@@ -9,10 +9,12 @@ use Illuminate\Http\Request;
 class RealtorListingAcceptOfferController extends Controller
 {
    public function __invoke(Offer $offer) {
+      $listing = $offer->listing;
+      $this->authorize("update", $offer->listing);
       $offer->update(["accepted_at" => now()]);
-      $offer->listing->offers()->except($offer)->update(["rejected_at" => now()]);
-      $offer->listing->sold_at = now();      
-      $offer->listing->save();      
+      $listing->offers()->except($offer)->update(["rejected_at" => now()]);
+      $listing->sold_at = now();      
+      $listing->save();      
 
 
       return redirect()->back()->with([
